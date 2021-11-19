9443dd6807538f0ef0777dacbb1245b995d02d21
Theo da Montanha <68796955+theodamontanha@users.noreply.github.com>
2021-11-18 23:58:26 -0300

    Update instalacoes_basicas.sh

--- instalacoes_basicas.sh
+++ instalacoes_basicas.sh
@@ -1,11 +1,19 @@
-useradd -m -G wheel -s /bin/bash <usuario>
-passwd <usuario>
+echo "Digite o nome do usuario padrao [NÃO root]:"
+read usuario
+echo "O nome de usuario selecionado foi" $usuario
+useradd -m -G wheel -s /bin/bash $usuario
+echo "Digite a senha para o usuario:"
+passwd $usuario
+echo "Digite a senha para o root: "
+passwd
 systemctl start ssh.service
-pacman -Syy
-pacman -S networkmanager network-manager-applet xorg-server xorg-xinit lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings i3-gaps i3status i3lock dmenu xfce4 alacritty nautilus chromium samba ntfs-3g
-pacman -S vim nano dialog wireless_tools efibootmgr grub os-prober net-tools git go
+systemctl enable ssh.service
+pacman -Syy --noconfirm
+pacman --noconfirm -S networkmanager network-manager-applet xorg-server xorg-xinit lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings i3-gaps i3status i3lock dmenu xfce4 alacritty nautilus chromium samba ntfs-3g
+pacman --noconfirm -S vim nano dialog wireless_tools efibootmgr grub os-prober net-tools git go
 systemctl start NetworkManager
 systemctl enable NetworkManager
 systemctl enable lightdm
+su $usuario && cd ~
 git clone https://aur.archlinux.org/yay.git
-su <usuario> && cd yay && makepkg -si
+cd yay && makepkg -si
