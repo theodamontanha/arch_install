b6ed0804efdabac881986e8ebd58491c4e12ffdc
Theo da Montanha <68796955+theodamontanha@users.noreply.github.com>
2021-11-19 00:02:49 -0300

    Update instalacoes_basicas.sh

--- instalacoes_basicas.sh
+++ instalacoes_basicas.sh
@@ -11,9 +11,12 @@ systemctl enable ssh.service
 pacman -Syy --noconfirm
 pacman --noconfirm -S networkmanager network-manager-applet xorg-server xorg-xinit lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings i3-gaps i3status i3lock dmenu xfce4 alacritty nautilus chromium samba ntfs-3g
 pacman --noconfirm -S vim nano dialog wireless_tools efibootmgr grub os-prober net-tools git go
+grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=grub
+grub-mkconfig -o /boot/grub/grub.cfg
 systemctl start NetworkManager
 systemctl enable NetworkManager
 systemctl enable lightdm
 su $usuario && cd ~
 git clone https://aur.archlinux.org/yay.git
 cd yay && makepkg -si
+echo "Aparentemente foi tudo concluído com sucesso! "
